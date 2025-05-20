06028f3f3e8aee13b919114cdfc79187c7577586
PiotrO9 <piotr.ostrowski02@wp.pl>
2025-05-20 19:29:22 +0200

    feat: add button component

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,9 +1,56 @@
 <script setup lang="ts">
 // TODO
+
+interface Props {
+	variant: 'primary',
+	text: string
+	link?: string
+}
+
+withDefaults(defineProps<Props>(), {
+	variant: 'primary',
+	text: 'Button'
+})
 </script>
 
 <template>
-	<button>
-
+	<button :class="variant" class="button">
+		<a v-if="link" :href="link" class="text">
+			{{ text }}
+		</a>
+		<span v-else class="text">
+			{{ text }}
+		</span>
 	</button>
 </template>
+
+<style scoped>
+.button {
+	background-color: var(--color-primary);
+	color: white;
+	border-radius: 999px;
+	cursor: pointer;
+	outline: none;
+	border: none;
+	font-size: 16px;
+	font-weight: 700;
+	transition: translate 0.3s ease-in-out, background-color 0.3s ease-in-out;
+	text-align: center;
+	line-height: 1;
+	padding: 0;
+	margin: 0;
+
+	&:hover {
+		translate: 0 -3px;
+		background-color: var(--color-primary-hover);
+	}
+
+	.text {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: 16px 24px;
+		border-radius: 999px;
+	}
+}
+</style>
---
+++ src/views/DesignSystemView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import Button from '@/components/ui/Button.vue'
+</script>
+
+<template>
+	<Button variant="primary" text="Button" />
+	<Button variant="primary" text="Button" link="/" />
+</template>
